ea0e184ee54f99d1c010ef886c3c780b7ba8e653
Tomas Tomanek <tomas.tomanek7@gmail.com>
2015-09-16 04:27:54 +0200

    test
    add db connection

---
+++ SimplyDB/Database.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace SimplyDB
+{
+    public class Database
+    {
+        private SqlConnection Connection;
+        private string ConString = "Server=TOM-PC\\SQLEXPRESS;Database=SimplyDB;Trusted_Connection=yes;";
+
+        public Database()
+        {
+            Connection = new SqlConnection();
+        }
+
+        public void Connect ()
+        {
+            if(Connection.State != System.Data.ConnectionState.Open)
+            {
+                Connection.ConnectionString = ConString;
+                Connection.Open();
+            }
+        }
+        public void Close()
+        {
+            Connection.Close();
+        }
+
+        public int ExecuteNonQuery (SqlCommand command)
+        {
+            int rowNumber = 0;
+            try
+            {
+                command.Prepare();
+                rowNumber = command.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                Close();
+            }
+            return rowNumber;
+        }
+
+        public SqlCommand CreateCommmand(string strCommand)
+        {
+            SqlCommand command = new SqlCommand(strCommand, Connection);
+
+            return command;
+        }
+    }
+}
---
+++ SimplyDB/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimplyDB
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+        public int Age { get; set; }
+
+        public Person(string firstName, string lastName, string city, int age)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.City = city;
+            this.Age = age;
+        }
+
+        public override string ToString()
+        {
+            return FirstName + LastName;
+        }
+    }
+}
